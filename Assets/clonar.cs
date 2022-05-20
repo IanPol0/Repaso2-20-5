7ccc626804e56c68d9f439b1e407bf3dbca17eca
IanPol0 <ipolonuer@gmail.com>
2022-05-20 14:04:35 -0300

    Initial commit

---
+++ Assets/clonar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class clonar : MonoBehaviour
+{
+    public GameObject prefab;
+    public int counter;
+    public Text inputCloneAmount;
+    public Text ClonesLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0;
+        ClonesLeft.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClonePrefab()
+    {
+        if (counter < int.Parse(inputCloneAmount.text))
+        {
+            Instantiate(prefab);
+            counter++;
+            ClonesLeft.text = (int.Parse(inputCloneAmount.text) - counter).ToString();
+        }
+
+    }
+
+}
